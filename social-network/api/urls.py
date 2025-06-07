3721be4916e8a1d6bb14c2cf0a33fc4f08996e11
Vladyslav <154080171+metimol@users.noreply.github.com>
2025-06-07 13:07:45 +0200

    refactor: simplify viewsets and remove unused permissions and actions

--- social-network/api/urls.py
+++ social-network/api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
-from rest_framework_nested import routers
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 from . import views
 
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register('profiles', views.ProfileViewSet)
 router.register('posts', views.PostViewSet)
 
 
-posts_router = routers.NestedDefaultRouter(router, 'posts', lookup='post')
+posts_router = NestedDefaultRouter(router, 'posts', lookup='post')
 posts_router.register('comments', views.CommentViewSet, basename='post-comments')
 
 
@@ -16,10 +17,6 @@ app_name = 'api'
 
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('posts/following/', views.PostsFollowing.as_view(), name="posts-following"),
-    path('posts/bookmarks/', views.BookmarkedPosts.as_view(), name="posts-bookmarks"),
     path('', include(router.urls)),
     path('', include(posts_router.urls)),
 ]
